cdb9a8adfc17ca5a0966efbf5af5565aae68f5ee
Teruaki.Tsuji <47702668+TeruakiTsuji@users.noreply.github.com>
2026-02-14 12:40:49 +0900

    Delete app directory

--- app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, redirect, url_for, Flask, jsonify, render_template, flash, render_template_string, request,g, session
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    
-    return render_template('index.html')
